f64e61eb448a103045365780563b90f6c25803c1
Gabriel Philipe S <48845302+gabrielphilipes@users.noreply.github.com>
2025-02-23 11:25:49 -0300

    feat: notify to construction

---
+++ app/pages/index.vue
@@ -0,0 +1,5 @@
+<template>
+  <a href="https://github.com/gabrielphilipes/website" class="fixed top-0 left-0 w-full bg-red-500 text-white py-4 font-serif text-center hover:bg-red-600 ph_transition">
+    <span class="font-medium">Site em construção!</span> Clique aqui para acessar o GitHub e acompanhar o progresso 😊
+  </a>
+</template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,17 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-11-01',
-  devtools: { enabled: true }
-})
+  compatibilityDate: "2024-11-01",
+  devtools: { enabled: true },
+  future: { compatibilityVersion: 4 },
+  modules: ["@nuxtjs/tailwindcss", "@nuxtjs/google-fonts"],
+  srcDir: "app",
+  serverDir: "server",
+  css: ["~/assets/css/general.css"],
+
+  // Modules
+  googleFonts: {
+    families: {
+      Newsreader: [200, 300, 400, 500, 600, 700, 800],
+    },
+  },
+});
